bf7c2cd40c2060ee11e09c7ae3a86ce90e96f3fd
jim <jbkrazy77@gmail.com>
2026-07-29 12:37:42 -0300

    tst 4

--- app/src/main/java/com/frequencias/formas/MainActivity.java
+++ app/src/main/java/com/frequencias/formas/MainActivity.java
@@ -1,32 +1,15 @@
 package com.frequencias.formas;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 import android.app.Activity;               // ← ESSENCIAL
 import android.annotation.SuppressLint;
-=======
->>>>>>> 0f2b1aa (fix)
-=======
-import android.app.Activity;               // ← ESSENCIAL
-import android.annotation.SuppressLint;
->>>>>>> 4e5ba2c (fix)
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
-import androidx.appcompat.app.AppCompatActivity;
->>>>>>> 0f2b1aa (fix)
-
-public class MainActivity extends AppCompatActivity {
-=======
 public class MainActivity extends Activity {
->>>>>>> 4e5ba2c (fix)
 
     private WebView webView;
 
@@ -44,15 +27,7 @@ public class MainActivity extends Activity {
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
-<<<<<<< HEAD
-<<<<<<< HEAD
         settings.setMediaPlaybackRequiresUserGesture(false); // áudio automático
-=======
-        settings.setMediaPlaybackRequiresUserGesture(false);
->>>>>>> 0f2b1aa (fix)
-=======
-        settings.setMediaPlaybackRequiresUserGesture(false); // áudio automático
->>>>>>> 4e5ba2c (fix)
         settings.setAllowFileAccess(true);
         settings.setCacheMode(WebSettings.LOAD_DEFAULT);
 
@@ -95,15 +70,7 @@ public class MainActivity extends AppCompatActivity { // ou extends Activity
 
         webView.setWebChromeClient(new WebChromeClient());
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         // ATENÇÃO: verifique se o arquivo existe em app/src/main/assets/www/index.html
-=======
-        // Seu HTML está em app/src/main/assets/www/index.html
->>>>>>> 0f2b1aa (fix)
-=======
-        // ATENÇÃO: verifique se o arquivo existe em app/src/main/assets/www/index.html
->>>>>>> 4e5ba2c (fix)
         webView.loadUrl("file:///android_asset/www/index.html");
     }
 
